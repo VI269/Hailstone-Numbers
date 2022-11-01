1312ab1f2c93fc34d018208f92dfdf8373a19dbd
Vedant Iyer <111111107+VI269@users.noreply.github.com>
2022-11-01 18:28:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
-import matplotlib.pyplot as plt
-def hailstone_numbers(number):
-    curnum, lst = number, []
+try:
+    import matplotlib.pyplot as plt
+    def hailstone_numbers(number):
+        curnum, lst = number, []
+        while True:
+            lst.append(int(curnum))
+            if curnum % 2 == 0:  
+                curnum /= 2
+            else: 
+                curnum = (3*curnum)+1
+            if curnum == 1: 
+                return lst
     while True:
-        lst.append(int(curnum))
-        if curnum % 2 == 0:  
-            curnum /= 2
-        else: 
-            curnum = (3*curnum)+1
-        if curnum == 1: 
-            return lst
-while True:
-    num = int(input("Number: "))
-    x, y = [i for i in range(1, len(hailstone_numbers(num)))], [hailstone_numbers(num)[i-1] for i in range(1, len(hailstone_numbers(num)))]
-    plt.plot(x, y)
-    print("Steps: "+str(len(hailstone_numbers(num))))
-    if input("Q to quit or anything else to show graph: ").lower() == 'q':
-        quit()
-    plt.show()
+        num = int(input("Number: "))
+        x, y = [i for i in range(1, len(hailstone_numbers(num)))], [hailstone_numbers(num)[i-1] for i in range(1, len(hailstone_numbers(num)))]
+        plt.plot(x, y)
+        print("Steps: "+str(len(hailstone_numbers(num))))
+        if input("Q to quit or anything else to show graph: ").lower() == 'q':
+            quit()
+        plt.show()
+except:
+    print('Something went wrong')
